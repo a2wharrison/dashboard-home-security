a0d29f32ad6804768687d4730800afd19ec8f9bd
Muhammad Syamsul <emesbi.48@gmail.com>
2018-06-05 23:55:49 +0700

    fix page and other page and method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\User;
+use Session;
+use cookies;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function dashboard()
     {
-        return view('site.index');
+        $user = session('users');
+        $data['email'] = $user['email'];
+        return view('site.index', $data);
     }
 
     
@@ -50,22 +54,31 @@ class UserController extends Controller
 
     public function login()
     {
+
         return view('site.login');
+    }
+
+    public function logout(Request $request)
+    {
+      $request->session()->forget('users');
+
+      return redirect(url('/login'));
     }
 
     public function doLogin(Request $request)
     {
         $data = DB::table('users')
-            ->where(['username'=> $request->username])
+            ->where(['name'=> $request->username])
             ->first();
-        
+        // dd($request->password);
         if (Hash::check($request->password, $data->password)) {
           
           $data = (array)$data;
+          $user = session::put(['users'=>$data]);
           
           return response(['data'=>$data], 200);
         } else {
-          return response(['message'=> 'Username and password did not match'], 500);
+          return response(['message'=> 'Username and password did not match']);
         }
     }
 }
